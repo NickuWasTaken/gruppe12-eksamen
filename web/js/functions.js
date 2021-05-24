a1f2d40f7d5c2def3ede45b4e6c06b12000ad388
anders licht <anderslicht1@gmail.com>
2021-05-24 17:23:03 +0200

    burger implemented

--- web/js/functions.js
+++ web/js/functions.js
@@ -47,3 +47,30 @@ function stepByStep(currentStep){
 	console.log(titleSteps[id]);
 	console.log(textSteps[id]);
 }
+//burger menu js
+let menu = document.querySelector(".menu");
+let ham = document.querySelector(".ham");
+let xIcon = document.querySelector(".xIcon");
+let menuIcon = document.querySelector(".menuIcon");
+
+ham.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	if (menu.classList.contains("showMenu")) {
+		menu.classList.remove("showMenu");
+		xIcon.style.display = "none";
+		menuIcon.style.display = "block";
+	} else {
+		menu.classList.add("showMenu");
+		xIcon.style.display = "block";
+		menuIcon.style.display = "none";
+	}
+}
+
+let menuLinks = document.querySelectorAll(".menuLink");
+
+menuLinks.forEach(
+	function (menuLink) {
+		menuLink.addEventListener("click", toggleMenu);
+	}
+)
