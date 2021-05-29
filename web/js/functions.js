f7003b5fceb7b69703a1c0472ec8998da7129565
anders licht <anderslicht1@gmail.com>
2021-05-29 08:58:46 +0200

    styleguide måske fix og omvendt burger menu

--- web/js/functions.js
+++ web/js/functions.js
@@ -129,12 +129,12 @@ ham.addEventListener("click", toggleMenu);
 function toggleMenu() {
 	if (menu.classList.contains("showMenu")) {
 		menu.classList.remove("showMenu");
-		xIcon.style.display = "none";
-		menuIcon.style.display = "block";
-	} else {
-		menu.classList.add("showMenu");
 		xIcon.style.display = "block";
 		menuIcon.style.display = "none";
+	} else {
+		menu.classList.add("showMenu");
+		xIcon.style.display = "none";
+		menuIcon.style.display = "block";
 	}
 }
 
